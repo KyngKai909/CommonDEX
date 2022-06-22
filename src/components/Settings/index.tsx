80ccd134b91f4a7f1ee612b970b3d72875d330d8
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2022-06-22 12:05:25 -0700

    Update index.tsx

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -343,7 +343,7 @@ export default function SettingsTab() {
                       <TYPE.body fontWeight={700} fontSize="9px" letterSpacing="3px" color="text1" marginBottom="4px">
                         The People's CommonWealth
                       </TYPE.body>
-                      <TYPE.body fontWeight={500} fontSize="7x" letterSpacing="3px" color="text1">
+                      <TYPE.body fontWeight={500} fontSize="7px" letterSpacing="3px" color="text1">
                         COMMONDAO.XYZ
                       </TYPE.body>
                     </MenuBanner>
